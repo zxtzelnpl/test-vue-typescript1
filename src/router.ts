dfbaa9ffc1f63cbf1562fb4f38120f3253c580e3
zxt_z <zxt_zel_npl@163.com>
2019-11-19 22:28:47 +0800

    ******

--- src/router.ts
+++ src/router.ts
@@ -18,6 +18,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/demo-one',
+      name: 'demo-one',
+      component: () => import(/* webpackChunkName: "about" */ './views/DemoOne/index.vue')
     }
   ]
 })
---
+++ src/views/DemoOne/Child.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    child's message
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'DemoOneChild',
+  props: {
+    message: String
+  },
+  updated () {
+    console.log('demo-one-child');
+  }
+})
+</script>
